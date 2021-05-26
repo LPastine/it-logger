75ee941ad7e151cb8e495e8d915ba1d6f2bff542
Luciano Pastine <lucianopastine.tech@gmail.com>
2021-05-26 12:30:51 -0300

    Logs component className correction

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -23,7 +23,7 @@ export const Logs = () => {
     }
 
     return (
-        <ul className="collection-with-header">
+        <ul className="collection with-header">
             <li className="collection-header">
                 <h4 className="center">System Logs</h4>
             </li>
